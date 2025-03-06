6019a3462d59ab2b1cd0b71949a9f3fe46f1c2b8
Keerat <tanwarkeeratcs@gmail.com>
2025-03-05 22:59:59 -0600

    add a new project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,86 +5,78 @@ import band from "../../assets/band.png";
 import foodie from "../../assets/foodie.png";
 import laroye from "../../assets/laroye.avif";
 import anim from "../../assets/anim.png";
+import reginaroads from "../../assets/reginaroads.png";
 
 
 
 function Projects() {
 
+    const projects = [
+        {
+            imgpath: reginaroads, altTxt: "ReginaRoads- Report and View road conditions",
+            cap: "ReginaRoads- Report and View road conditions",
+            url: "https://regina-roads.vercel.app/"
+        },
+        {
+
+
+            imgpath: coffee, altTxt: "Coffee Shop",
+            cap: "Coffee Shop",
+            url: 'https://keerat-tanwar-coffeeshop.vercel.app/'
+        },
+        {
+            imgpath: band, altTxt: "Band Site",
+            cap: "Band Site",
+            url: "https://keerat-tanwar-bandsite.vercel.app/"
+        },
+        {
+            imgpath: laroye, altTxt: "laroye-mental health app",
+            cap: "laroye-mental health app",
+            url: "https://laroye.ai"
+        },
+        {
+            imgpath: anim, altTxt: "Anime recommender",
+            cap: "Anime recommender",
+            url: "https://anirecommender.vercel.app/"
+        },
+        {
+            imgpath: foodie, altTxt: "FoodieFind: Search and manage food",
+            cap: "FoodieFind: Search and manage food",
+            url: "http://3.128.33.252:5000/"
+        },
+
+    ]
+
 
     return (
         <div className='projects' id='projects'>
-            <TiltedCard
-                imageSrc="https://i.scdn.co/image/ab67616d0000b273d9985092cd88bffd97653b58"
-                altText="Kendrick Lamar - GNX Album Cover"
-                captionText="Kendrick Lamar - GNX"
-                containerHeight="300px"
-                containerWidth="400px"
-                imageHeight="300px"
-                imageWidth="400px"
-                rotateAmplitude={12}
-                scaleOnHover={1.2}
-                showMobileWarning={false}
-                showTooltip={true}
-                displayOverlayContent={true}
-                overlayContent={
-                    <p className="tilted-card-demo-text">
-                        ReginaRoads- Report and View road conditions
-                    </p>
-                }
-                urlClick="https://regina-roads.vercel.app/"
-            />
-            <TiltedCard
-                imageSrc={coffee}
-                altText="Coffee Shop"
-                captionText="Coffee Shop"
-                containerHeight="300px"
-                containerWidth="400px"
-                imageHeight="300px"
-                imageWidth="400px"
-                urlClick='https://keerat-tanwar-coffeeshop.vercel.app/'
-            />
-            <TiltedCard
-                imageSrc={band}
-                altText="Band Site"
-                captionText="Band Site"
-                containerHeight="300px"
-                containerWidth="400px"
-                imageHeight="300px"
-                imageWidth="400px"
-                urlClick="https://keerat-tanwar-bandsite.vercel.app/"
-            />
-            <TiltedCard
-                imageSrc={laroye}
-                altText="laroye-mental health app"
-                captionText="laroye-mental health app"
-                containerHeight="300px"
-                containerWidth="400px"
-                imageHeight="300px"
-                imageWidth="400px"
-                urlClick="https://laroye.ai"
+            {projects.map((project) => {
+                console.log(project);
+                return (
+                    <TiltedCard
+                        imageSrc={project["imgpath"]}
+                        altText={project["altTxt"]}
+                        captionText={project["cap"]}
+                        containerHeight="300px"
+                        containerWidth="400px"
+                        imageHeight="300px"
+                        imageWidth="400px"
+                        rotateAmplitude={12}
+                        scaleOnHover={1.2}
+                        showMobileWarning={false}
+                        showTooltip={true}
+                        displayOverlayContent={true}
+                        overlayContent={
+                            <p className="tilted-card-demo-text">
+                                {project["altTxt"]}
+                            </p>
+                        }
+                        urlClick="https://regina-roads.vercel.app/"
+                    />
+                )
 
-            />
-            <TiltedCard
-                imageSrc={anim}
-                altText="Anime recommender"
-                captionText="Anime recommender"
-                containerHeight="300px"
-                containerWidth="400px"
-                imageHeight="300px"
-                imageWidth="400px"
-                urlClick="https://anirecommender.vercel.app/"
+            })}
 
-            />
-            <TiltedCard
-                imageSrc={foodie}
-                altText="FoodieFind: Search and manage food"
-                captionText="FoodieFind: Search and manage food"
-                containerHeight="300px"
-                containerWidth="400px"
-                imageHeight="300px"
-                imageWidth="400px"
-                urlClick="http://3.128.33.252:5000/"
-            />
 
         </div>
     )
